f1f477894c460dae5bb36c186313cfe635b4043f
enriquemanzanov <cesar.manzano@ayte.co>
2024-11-19 14:50:48 -0500

    [FIX] comment Trigger tests

--- tests/unit/test_cdk_fargate_deploy_stack.py
+++ tests/unit/test_cdk_fargate_deploy_stack.py
@@ -5,10 +5,13 @@ from cdk_fargate_deploy.cdk_fargate_deploy_stack import CdkFargateDeployStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_fargate_deploy/cdk_fargate_deploy_stack.py
-def test_sqs_queue_created():
+@pytest.fixture
+def CdkFargateDeployStack():
     app = core.App()
     stack = CdkFargateDeployStack(app, "cdk-fargate-deploy")
     template = assertions.Template.from_stack(stack)
+    return template
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
