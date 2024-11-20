64f280620faea44b1daef3578e3416d6b60073a0
enriquemanzanov <cesar.manzano@ayte.co>
2024-11-20 13:12:07 -0500

    [FIX]/integration uri in integration

--- cdk_fargate_deploy/cdk_fargate_deploy_stack.py
+++ cdk_fargate_deploy/cdk_fargate_deploy_stack.py
@@ -67,7 +67,7 @@ class CdkFargateDeployStack(Stack):
             description="API Integration with AWS Fargate Service",
             integration_method="GET",
             integration_type="HTTP_PROXY",
-            integration_uri= service.load_balancer.load_balancer_arn,
+            integration_uri= service.listener.listener_arn, 
             payload_format_version="1.0"
         )
         
