694e75ac458fc957986f9761d1e58f81d7c95abc
enriqueman <manzano.velasco217@gmail.com>
2025-05-01 14:40:47 -0500

    [FIX]/cdk env

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ CdkFargateDeployStack(app, "CdkFargateDeployStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    env=cdk.Environment(account='767398062810', region='us-east-2'),
+    env=cdk.Environment(account='471112837636', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
